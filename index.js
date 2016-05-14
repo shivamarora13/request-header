227fe5cb2a9909b64e64df39ab79aa8abb191f68
Shivam Arora <shivamarora1131@gmail.com>
2016-05-15 03:13:05 +0530

    done

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
 var express = require('express');
+var address = require('address');
+var os = require('os');
+var osName = require('os-name');
+var getBrowserLanguage = require('get-browser-language');
+var lang = getBrowserLanguage();
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/public'));
-
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-
 app.get('/', function(request, response) {
-  response.render('pages/index');
+
+  var ip = address.ip();
+  var osname = osName(os.platform(),os.release());
+  var langu = lang();
+
+  var obj = {
+  	ipaddress : ip,
+  	language : langu,
+  	software : osname
+  }
+
+  response.send(JSON.strigify(obj));
 });
 
 app.listen(app.get('port'), function() {
